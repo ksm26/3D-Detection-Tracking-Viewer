38d71c8e15af217fa3c44ee105d6d74b4b46d6ad
Abhishek Tomy <abhishek.tomy@inria.fr>
2022-12-13 15:58:27 +0100

    Viewer kitti

--- kitti_3D_tracking_viewer.py
+++ kitti_3D_tracking_viewer.py
@@ -1,11 +1,11 @@
 from viewer.viewer import Viewer
 import numpy as np
-import re 
 from dataset.kitti_dataset import KittiTrackingDataset
+import re
 
 def kitti_viewer():
-    root="/home/khushdeep/Desktop/3D-Multi-Object-Tracker/kitti_tracking/training"
-    label_path = f"{root}/label_02/0012.txt"
+    root="/media/storage/kitti/kitti_training"
+    label_path = f"/home/Abhishek/Deep_networks/3D-Multi-Object-Tracker/evaluation/results/sha_key/data/0011.txt"
     sequence_id = int(re.findall(r'\d+', label_path)[-1])
     dataset = KittiTrackingDataset(root,seq_id=sequence_id,label_path=label_path)
 
@@ -14,19 +14,21 @@ def kitti_viewer():
     for i in range(len(dataset)):
         P2, V2C, points, image, labels, label_names = dataset[i]
 
+
         if labels is not None:
             mask = (label_names=="Car")
             labels = labels[mask]
             label_names = label_names[mask]
-            vi.add_3D_boxes(labels, ids=labels[:, -1].astype(int), box_info=label_names,caption_size=(0.09,0.09))
+            vi.add_3D_boxes(labels, ids=labels[:, -1].astype(int), box_info=label_names,caption_size=(0.09,0.09),show_heading = False)
             vi.add_3D_cars(labels, ids=labels[:, -1].astype(int), mesh_alpha=1)
-        vi.add_points(points[:,:3])
+        # vi.add_points(points[:,:3])
 
         vi.add_image(image)
         vi.set_extrinsic_mat(V2C)
         vi.set_intrinsic_mat(P2)
 
-        vi.show_2D(sequence_id, i)
+        vi.show_2D()
+        # vi.save_2D()
 
         # vi.show_3D()
 
